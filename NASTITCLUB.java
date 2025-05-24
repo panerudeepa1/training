80a3bd13f3a42ca4f23bd4815d1f445dd47836d6
Deepa Paneru <deepapaneru220@gmail.com>
2025-05-24 11:35:32 +0545

    a lots of thanks!

--- NASTITCLUB.java
+++ NASTITCLUB.java
@@ -1,6 +1,6 @@
 public class NastItClub {
     public static void main(String... args){
-        System.out.println("thank u so much much NAST IT club for organizing 3 days git and github workshop");
+        System.out.println("thank u so much much NAST IT club for organizing  experimental 3 days git and github workshop");
 
     }
 }
