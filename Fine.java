d2973f5d4b3615fbdfadbc10b406126bc8465334
Deepa Paneru <deepapaneru220@gmail.com>
2025-05-24 11:00:18 +0545

    health is wealth

---
+++ Fine.java
@@ -0,0 +1,8 @@
+public class Fine {
+    public static void main()
+    {
+        System.out.println("i am fine today rather than yesterday");
+        System.out.println("if a person is healthy ,he/she can win the whole world");
+    }
+    
+}
